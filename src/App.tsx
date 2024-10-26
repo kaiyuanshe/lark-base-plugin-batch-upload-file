fea1471804103c0bf3dd69d433bfc50fe1541804
stevending1st <stevending1st@163.com>
2024-10-26 14:32:59 +0800

    feat: add check and upload file

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,204 @@
 import './App.css';
-import { bitable, ITableMeta } from "@lark-base-open/js-sdk";
-import { Button, Form } from '@douyinfe/semi-ui';
+import { bitable, FieldType, ToastType } from "@lark-base-open/js-sdk";
+import { Button, Switch, Form, Typography, Tabs, TabPane, Input } from '@douyinfe/semi-ui';
 import { BaseFormApi } from '@douyinfe/semi-foundation/lib/es/form/interface';
 import { useState, useEffect, useRef, useCallback } from 'react';
 
+export type SelectionType = Record<"baseId" | "fieldId" | "recordId" | "tableId" | "viewId", string>
+
+const wait = (seconds: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
+}
+
 export default function App() {
-  const [tableMetaList, setTableMetaList] = useState<ITableMeta[]>();
+  const [selectionInfo, setSectionInfo] = useState<SelectionType>();
+  const [webHookUrl, setWebHookUrl] = useState<String>(localStorage.webHookUrl);
+  const [baseToken, setBaseToken] = useState<String>(localStorage.baseToken);
+  const [isBatch, setIsBatch] = useState(false);
   const formApi = useRef<BaseFormApi>();
-  const addRecord = useCallback(async ({ table: tableId }: { table: string }) => {
-    if (tableId) {
-      const table = await bitable.base.getTableById(tableId);
-      table.addRecord({
-        fields: {},
-      });
+
+  useEffect(() => {
+    const offSelectionChange = bitable.base.onSelectionChange(({ data }) =>
+      setSectionInfo(() => data as SelectionType)
+    )
+
+    return offSelectionChange
+  })
+
+  const checkIsFileUploaded = useCallback(async (fileName: string) => {
+    try {
+      const response = await fetch(`https://ows.blob.core.chinacloudapi.cn/$web/file/${fileName}`);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      return true;
+    } catch (error: any) {
+      bitable.ui.showToast({
+        toastType: ToastType.error,
+        message: error.message
+      })
+      return false;
     }
   }, []);
-  useEffect(() => {
-    Promise.all([bitable.base.getTableMetaList(), bitable.base.getSelection()])
-      .then(([metaList, selection]) => {
-        setTableMetaList(metaList);
-        formApi.current?.setValues({ table: selection.tableId });
-      });
-  }, []);
+
+
+  const uploadFile = ({ baseId, tableId, recordId }: SelectionType) =>
+    fetch(`https://service.kaiyuanshe.cn/crawler/task/lark/base/${baseId}/${tableId}/${recordId}/file`, {
+      method: "POST", headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${baseToken}`,
+      }
+    });
+
+  const postBotMsg = async ({ imgURL }: Record<"imgURL", string>) => {
+    if (!webHookUrl) return;
+
+    await fetch(webHookUrl as any as URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "msg_type": "interactive",
+        "card": {
+          "elements": [
+            {
+              "tag": "div",
+              "text": {
+                "content": `**存储地址：** [${imgURL}](${imgURL})`,
+                "tag": "lark_md"
+              }
+            },
+          ],
+          "header": {
+            "template": "green",
+            "title": {
+              "content": "✅失效图片重新上传成功",
+              "tag": "plain_text"
+            }
+          }
+        }
+      }),
+    })
+  }
+
+  const checkAndUpload = async ({ baseId, tableId, recordId, fieldId, viewId }: SelectionType) => {
+    const table = await bitable.base.getTable(tableId);
+    const field = await table.getField(fieldId);
+
+    const getFieldType = await field.getType();
+    // 检查是否是文件类型
+    if (getFieldType !== FieldType.Attachment) {
+      bitable.ui.showToast({
+        toastType: ToastType.error,
+        message: "NOt A File"
+      })
+      return;
+    }
+
+    const cell = await field.getCell(recordId);
+    const value = await cell.getValue();
+    const fileName = value[0]?.name;
+    if (!fileName) return;
+
+    // 如果没有上传则上传
+    if (!await checkIsFileUploaded(fileName)) {
+      await uploadFile({ baseId, tableId, recordId, fieldId, viewId });
+      postBotMsg({ imgURL: `https://ows.blob.core.chinacloudapi.cn/$web/file/${fileName}` })
+    }
+  }
+
+  const batchCheckAndUpload = async ({ baseId, tableId, recordId, fieldId, viewId }: SelectionType) => {
+    console.log("🚧批量程序，启动！")
+
+    const table = await bitable.base.getTable(tableId);
+    const field = await table.getField(fieldId);
+
+    const getFieldType = await field.getType();
+    // 检查是否是文件类型
+    if (getFieldType !== FieldType.Attachment) {
+      bitable.ui.showToast({
+        toastType: ToastType.error,
+        message: "NOt A File"
+      })
+      return;
+    }
+
+    const recordList = await table.getRecordList();
+    console.log("recordList:", recordList)
+    let i = 1;
+    for (const record of recordList) {
+      const cell = await record.getCellByField(fieldId);
+      const value = await cell.getValue();
+      if (!value?.length) continue;
+      const fileName = value?.[0]?.name;
+      console.log(record.id, fieldId, "--fileName:", fileName)
+      if (!fileName) return;
+      // 如果没有上传则上传
+      if (!await checkIsFileUploaded(fileName)) {
+        await uploadFile({ baseId, tableId, recordId: record.id, fieldId, viewId });
+        postBotMsg({ imgURL: `https://ows.blob.core.chinacloudapi.cn/$web/file/${fileName}` })
+
+        await wait(1);
+      }
+    }
+
+    console.log("✅跑完了！")
+  }
 
   return (
     <main className="main">
-      <h4>
-        Edit <code>src/App.tsx</code> and save to reload
-      </h4>
-      <Form labelPosition='top' onSubmit={addRecord} getFormApi={(baseFormApi: BaseFormApi) => formApi.current = baseFormApi}>
-        <Form.Slot label="Development guide">
-          <div>
-            <a href="https://lark-technologies.larksuite.com/docx/HvCbdSzXNowzMmxWgXsuB2Ngs7d" target="_blank"
-              rel="noopener noreferrer">
-              Base Extensions Guide
-            </a>
-            、
-            <a href="https://bytedance.feishu.cn/docx/HazFdSHH9ofRGKx8424cwzLlnZc" target="_blank"
-              rel="noopener noreferrer">
-              多维表格插件开发指南
-            </a>
-          </div>
-        </Form.Slot>
-        <Form.Slot label="API">
-          <div>
-            <a href="https://lark-technologies.larksuite.com/docx/Y6IcdywRXoTYSOxKwWvuLK09sFe" target="_blank"
-              rel="noopener noreferrer">
-              Base Extensions Front-end API
-            </a>
-            、
-            <a href="https://bytedance.feishu.cn/docx/HjCEd1sPzoVnxIxF3LrcKnepnUf" target="_blank"
-              rel="noopener noreferrer">
-              多维表格插件API
-            </a>
-          </div>
-        </Form.Slot>
-        <Form.Select field='table' label='Select Table' placeholder="Please select a Table" style={{ width: '100%' }}>
-          {
-            Array.isArray(tableMetaList) && tableMetaList.map(({ name, id }) => {
-              return (
-                <Form.Select.Option key={id} value={id}>
-                  {name}
-                </Form.Select.Option>
-              );
-            })
-          }
-        </Form.Select>
-        <Button theme='solid' htmlType='submit'>Add Record</Button>
-      </Form>
+      <div style={{ display: 'flex', flexDirection: "column" }}>
+        <Typography.Title heading={6} style={{ margin: 8 }}>
+          Bot webhook
+        </Typography.Title>
+        <Input placeholder='https://open.feishu.cn/open-apis/bot/v2/hook/...' size='large' onChange={(value) => setWebHookUrl(value)} />
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: "column" }}>
+        <Typography.Title heading={6} style={{ margin: 8 }}>
+          Lark Base Token
+        </Typography.Title>
+        <Input placeholder='Authorization' size='large' onChange={(value) => setBaseToken(value)} />
+      </div>
+
+      <Tabs type="card" style={{ margin: "20px 0" }}>
+        <TabPane tab="裁剪图片" itemKey="1" style={{ margin: "20px 0" }}>
+          文档
+        </TabPane>
+        <TabPane tab="批量检查" itemKey="2" style={{ margin: "20px 0" }}>
+          <Form labelPosition='top' onSubmit={() => selectionInfo && (isBatch ? batchCheckAndUpload(selectionInfo) : checkAndUpload(selectionInfo))} getFormApi={(baseFormApi: BaseFormApi) => formApi.current = baseFormApi}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Typography.Title heading={6} style={{ margin: 8 }}>
+                {isBatch ? '批量处理' : '单个处理'}
+              </Typography.Title>
+              <Switch checked={isBatch} onChange={(value) => setIsBatch(value)} />
+            </div>
+
+
+            {selectionInfo?.fieldId && selectionInfo?.recordId &&
+              <Form.Slot label="选中单元格信息">
+                <ul>
+                  <li>baseId: {selectionInfo.baseId}</li>
+                  <li>fieldId: {selectionInfo.fieldId}</li>
+                  <li>recordId: {selectionInfo.recordId}</li>
+                  <li>tableId: {selectionInfo.tableId}</li>
+                  <li>viewId: {selectionInfo.viewId}</li>
+                </ul>
+              </Form.Slot>
+            }
+
+            <Button disabled={!selectionInfo?.fieldId || !selectionInfo?.recordId || !baseToken} theme='solid' htmlType='submit'>{isBatch ? "批量" : "单个"}检测</Button>
+          </Form>
+        </TabPane>
+        <TabPane tab="帮助" itemKey="3">
+          帮助
+        </TabPane>
+      </Tabs>
     </main>
   )
 }
